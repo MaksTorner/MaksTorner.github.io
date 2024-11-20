08dae7939ec8f537d2ada06d86e589370a0a7d7a
MaksTorner <148188261+MaksTorner@users.noreply.github.com>
2024-11-20 09:06:09 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDcYOcK6clew7z--w8kpkyaVFZ0FfDEWos",
+  authDomain: "MaksTorner.Github.io",
+  projectId: "makswolfportfolio",
+  storageBucket: "projektfiler",
+  messagingSenderId: "205736234049",
+  appId: "1:205736234049:web:e0291670222587b24a4ac0"
+};
+
+firebase.initializeApp(firebaseConfig);
